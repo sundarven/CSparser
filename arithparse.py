ac0addd46750d91eb34de6a5bbad2157587e07bf
sundarven <103016358+sundarven@users.noreply.github.com>
2022-04-05 00:01:14 -0400

    Add files via upload

---
+++ arithparse.py
@@ -0,0 +1,57 @@
+import operator
+#import pyparsing
+
+
+
+def parse(x):
+
+    # TODO parantheses grouping and recursive calculation
+    if '(' in x or ')' in x:
+        assert( (x.count('(') - x.count(')')) == 0),'Parantheses mismatch'
+
+    
+    operators = set('+-*/')
+    op_out = []    #This holds the operators that are found in the string (left to right)
+    num_out = []   #This holds the non-operators that are found in the string (left to right)
+    buff = []
+    for c in x:  #examine 1 character at a time
+        if c in operators:  
+            #found an operator.  Everything we've accumulated in `buff` is 
+            #a single "number". Join it together and put it in `num_out`.
+            num_out.append(''.join(buff))
+            buff = []
+            op_out.append(c)
+        else:
+            #not an operator.  Just accumulate this character in buff.
+            buff.append(c)
+    num_out.append(''.join(buff))
+    return num_out,op_out
+
+
+def my_eval(nums,ops):
+
+    nums = list(nums)
+    ops = list(ops)
+    operator_order = ('*/','+-')  #precedence from left to right.  operators at same index have same precendece.
+                                  #map operators to functions.
+    op_dict = {'*':operator.mul,
+               '/':operator.floordiv,
+               '+':operator.add,
+               '-':operator.sub}
+    Value = None
+    for op in operator_order:                   #Loop over precedence levels
+        while any(o in ops for o in op):        #Operator with this precedence level exists
+            idx,oo = next((i,o) for i,o in enumerate(ops) if o in op) #Next operator with this precedence         
+            ops.pop(idx)                        #remove this operator from the operator list
+            values = map(float,nums[idx:idx+2]) #here I just assume float for everything
+            value = op_dict[oo](*values)
+            nums[idx:idx+2] = [value]           #clear out those indices
+
+    return nums[0]
+
+
+if __name__ == "__main__":
+    print(my_eval(*parse('3/2*15')))
+    
+
+
